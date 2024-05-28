0cde114a45fb0ae501dd48191223453431c2a33a
Peter Zhang <zozohtnt@gmail.com>
2024-05-29 02:24:09 +0800

    ...

--- src/lib/_top/sql-types.ts
+++ src/lib/_top/sql-types.ts
@@ -1,4 +1,4 @@
-import { NameAnyValue } from '@site0/tijs';
+import { NameAnyValue, Vars } from '@site0/tijs';
 import { QueryFilter, QuerySorter } from '..';
 
 export type SqlPager = {
@@ -27,15 +27,20 @@ export type SqlQuery = {
 export type SqlExecOptions = {
   /**
    * 执行完创建语句，还需要把这个最新的插入记录读取回来
-   * 读取的方式由这个属性来指明，譬如
+   * 读取的方式由这个属性来指明。 这个选项的值是一个元组：
+   *
+   * - 1. 表示一个 SQL 语句名称
+   * - 2. 这个查询语句的输入参数，通常是一个组合条件
+   *
+   * 组合条件就是一个查询条件表，譬如
    *
    * ```
-   * {name:'nickname', value:'litter red'}
+   * {name:'litter red'}
    * ```
-   * 或者，联合主键
+   * 或者多个条件
    *
    * ```
-   * [{name:'nickname', value:'red'},{name:'race', value:'dog'}]
+   * {name:'red', race:'dog'}
    * ```
    *
    * 给定的条件，如果查询出多个结果，服务器会抛错误
@@ -44,11 +49,11 @@ export type SqlExecOptions = {
    * 服务器动态生成的，在客户端没法传递，因此支持 explain 的写法:
    *
    * ```
-   * {name:'id', value:'=id'}
+   * {id:'=id'}
    * ```
    *
    * 这个例子表示，执行完插入语句，紧接着会从服务器的上下文里，直接获取 'id' 的值，
    * 再查询回来
    */
-  fetchBack?: NameAnyValue | NameAnyValue[];
+  fetchBack?: [string, Vars];
 };
