d1681b0d67d268cc0b303f3b4aeadd7dc8493922
Peter Zhang <zozohtnt@gmail.com>
2026-07-16 22:37:26 +0800

    refactor(WnObjTitle): 改用全局提示API替代注入

--- src/lib/input/obj-title/WnObjTitle.vue
+++ src/lib/input/obj-title/WnObjTitle.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
-  import { TI_TIP_API_KEY, TiActionBar, TiIcon } from "@site0/tijs";
-  import { computed, inject, onUnmounted, useTemplateRef, watch } from "vue";
+  import { TI_APP_TIPS, TiActionBar, TiIcon } from "@site0/tijs";
+  import { computed, onUnmounted, useTemplateRef, watch } from "vue";
   import { useWnObjTitleActions } from "./use-wn-obj-title-actons";
   import { useWnObjTitleApi } from "./use-wn-obj-title-api";
   import { WnObjTitleEmitter, WnObjTitleProps } from "./wn-obj-title-types";
   //-----------------------------------------------------
   const $text = useTemplateRef("text");
-  const _tip_api = inject(TI_TIP_API_KEY);
+  const _tip_api = TI_APP_TIPS.api;
   //-----------------------------------------------------
   const emit = defineEmits<WnObjTitleEmitter>();
   const props = withDefaults(defineProps<WnObjTitleProps>(), {});
